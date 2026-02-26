cf51bddfc37d96963219adb2eaa2be96f85840c9
dedybayu <dedybayusetiawan5@gmail.com>
2026-02-26 11:21:29 +0700

    coba

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
     setError("")
 
     try {
-      const response = await fetch("http://localhost:8085/auth/login", {
+      const response = await fetch("http://10.10.168.17:8085/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
